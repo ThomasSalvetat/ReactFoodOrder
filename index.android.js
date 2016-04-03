8b8b8a42baf24c5422ca12073075c1d500b1e5ed
Thomas SALVETAT <thomas.salvetat@outlook.com>
2016-04-03 21:47:06 +0200

    Material design

--- index.android.js
+++ index.android.js
@@ -1,175 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+import React, { AppRegistry, Component, Navigator, DrawerLayoutAndroid, ScrollView, View, Text } from 'react-native';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Component,
-  Text,
-  View,
-  Navigator,
-  TouchableOpacity,
-  BackAndroid,
-  DrawerLayoutAndroid,
-  Image,
-  TouchableHighlight,
-} = React;
+import Navigate from './src/utils/Navigate';
+import { Toolbar } from './src/components';
+import Navigation from './src/scenes/Navigation';
+import Welcome from './src/scenes/Welcome';
 
-var SplashPage = require('./SplashPage');
-var LoginPage = require('./LoginPage');
-var MainPage = require('./MainPage');
-var PersonPage = require('./PersonPage');
-var NoNavigatorPage = require('./NoNavigatorPage');
-var NicothaiHomePage = require('./NicothaiHomePage');
-var PapilleHomePage = require('./PapilleHomePage');
-var _navigator;
-var _drawerWidth = 200;
+class Application extends Component {
 
-class App extends Component {
-  render() {
-    var navigationView = (
-        <View style={{flex: 1, backgroundColor: '#fff', alignItems: 'center'}}>
-            <Image style={styles.drawerIcon} source={require('./restau.png')} />
-            <TouchableHighlight style={styles.touchableHighlight} underlayColor="#6495ed"
-                onPress={this.navNicothaiHome.bind(this)}>
-                <Text style={{fontSize: 15, textAlign: 'left'}}>Nicothaï</Text>
-            </TouchableHighlight>
-            <TouchableHighlight style={styles.touchableHighlight} underlayColor="#6495ed"
-                onPress={this.navPapilleHome.bind(this)}>
-                <Text style={{fontSize: 15, textAlign: 'left'}}>Papille</Text>
-            </TouchableHighlight>
-        </View>
-        );
-    return (
-        <DrawerLayoutAndroid
-            drawerWidth={_drawerWidth}
-            ref={'DRAWER_REF'}
-            drawerPosition={DrawerLayoutAndroid.positions.Left}
-            renderNavigationView={() => navigationView}>
-            <Navigator
-                initialRoute={{id: 'SplashPage', name: 'Index'}}
-                renderScene={this.renderScene.bind(this)}
-                configureScene={(route) => {
-                if (route.sceneConfig) {
-                    return route.sceneConfig;
-                }
-                return Navigator.SceneConfigs.FloatFromRight;
-                }} />
-        </DrawerLayoutAndroid>
-    );
-  }
-  renderScene(route, navigator) {
-    _navigator = navigator;
-    var routeId = route.id;
-    if (routeId === 'SplashPage') {
-      return (
-        <SplashPage
-          navigator={navigator} />
-      );
-    }
-    if (routeId === 'LoginPage') {
-      return (
-        <LoginPage
-          navigator={navigator} />
-      );
-    }
-    if (routeId === 'MainPage') {
-      return (
-        <MainPage
-            navigator={navigator} />
-      );
-    }
-    if (routeId === 'PersonPage') {
-      return (
-        <PersonPage
-          navigator={navigator} />
-      );
-    }
-    if (routeId === 'NoNavigatorPage') {
-      return (
-        <NoNavigatorPage
-            navigator={navigator} />
-      );
-    }
-    if (routeId === 'NicothaiHomePage') {
-      return (
-        <NicothaiHomePage
-            navigator={navigator} />
-      );
-    }
-    if (routeId === 'PapilleHomePage') {
-      return (
-        <PapilleHomePage
-            navigator={navigator} />
-      );
-    }
-    return this.noRoute(navigator);
+	static childContextTypes = {
+		drawer: React.PropTypes.object,
+		navigator: React.PropTypes.object
+	};
 
-  }
-  noRoute(navigator) {
-    return (
-      <View style={{flex: 1, alignItems: 'stretch', justifyContent: 'center'}}>
-        <TouchableOpacity style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
-            onPress={() => navigator.pop()}>
-          <Text style={{color: 'red', fontWeight: 'bold'}}>No route</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-  navNicothaiHome() {
-    _navigator.push({
-      id: 'NicothaiHomePage',
-      name: 'NicothaiHomePage',
-    });
-  }
-  navPapilleHome() {
-    _navigator.push({
-      id: 'PapilleHomePage',
-      name: 'PapilleHomePage',
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			drawer: null,
+			navigator: null
+		};
+	}
+
+	getChildContext = () => {
+		return {
+			drawer: this.state.drawer,
+			navigator: this.state.navigator
+		}
+	};
+
+	setDrawer = (drawer) => {
+		this.setState({
+			drawer
+		});
+	};
+
+	setNavigator = (navigator) => {
+		this.setState({
+			navigator: new Navigate(navigator)
+		});
+	};
+
+	render() {
+		const { drawer, navigator } = this.state;
+		const navView = React.createElement(Navigation);
+
+		return (
+			<DrawerLayoutAndroid
+				drawerWidth={300}
+				drawerPosition={DrawerLayoutAndroid.positions.Left}
+				renderNavigationView={() => {
+                    if (drawer && navigator) {
+                        return navView;
+                    }
+                    return null;
+                }}
+				ref={(drawer) => { !this.state.drawer ? this.setDrawer(drawer) : null }}
+			>
+				{drawer &&
+				<Navigator
+					initialRoute={Navigate.getInitialRoute()}
+					navigationBar={<Toolbar onIconPress={drawer.openDrawer} />}
+					configureScene={() => {
+                            return Navigator.SceneConfigs.FadeAndroid;
+                        }}
+					ref={(navigator) => { !this.state.navigator ? this.setNavigator(navigator) : null }}
+					renderScene={(route) => {
+                        if (this.state.navigator && route.component) {
+                            return (
+                                <View
+                                    style={styles.scene}
+                                    showsVerticalScrollIndicator={false}>
+                                	<route.component title={route.title} path={route.path} {...route.props} />
+                                </View>
+                            );
+                        }
+                    }}
+				/>
+				}
+			</DrawerLayoutAndroid>
+		);
+	}
 }
 
-BackAndroid.addEventListener('hardwareBackPress', () => {
-  if (_navigator.getCurrentRoutes().length === 1  ) {
-     return false;
-  }
-  _navigator.pop();
-  return true;
-});
+AppRegistry.registerComponent('ReactFoodOrder', () => Application);
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  drawerIcon: {
-      width: 50, 
-      height: 50,
-      margin: 10,
-  },
-  touchableHighlight: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: _drawerWidth,
-  },
-});
-
-AppRegistry.registerComponent('ReactFoodOrder', () => App);
+const styles = {
+	scene: {
+		flex: 1,
+		marginTop: 56
+	}
+};
--- index.ios.js
+++ index.ios.js
@@ -1,36 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { AppRegistry, Component, StyleSheet, Text, View } from 'react-native';
 
-class ReactFoodOrder extends Component {
+class DemoApp extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          React Native Material Design
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          iOS example coming soon... PRs welcome!
         </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -46,7 +31,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
+  }
 });
 
-AppRegistry.registerComponent('ReactFoodOrder', () => ReactFoodOrder);
+AppRegistry.registerComponent('ReactFoodOrder', () => DemoApp);
